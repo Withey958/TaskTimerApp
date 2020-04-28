c0092af1a8a9d99f2388b27950ffd05c658e0668
luke withey <lukewithey@lukes-MacBook-Pro.local>
2020-04-28 10:46:08 +0100

    Add UI to app

--- app/src/main/java/lukewithey/example/tasktimer/MainActivity.kt
+++ app/src/main/java/lukewithey/example/tasktimer/MainActivity.kt
@@ -3,7 +3,6 @@ package lukewithey.example.tasktimer
 import android.content.ContentValues
 import android.os.Bundle
 import android.util.Log
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
@@ -18,8 +17,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        testInsert()
-        testUpdate()
+//        testInsert()
+//        testUpdate()
+//        testUpdateTwo()
+//        testDelete()
 
         val projection = arrayOf(TasksContract.Columns.TASK_NAME, TasksContract.Columns.TASK_SORT_ORDER)
         val sortColumn = TasksContract.Columns.TASK_SORT_ORDER
@@ -49,6 +50,41 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    // *********** test functions ****************
+
+    private fun testDeleteTwo() {
+
+        val selection = TasksContract.Columns.TASK_SORT_ORDER + " = ?"
+        val selectionArgs = arrayOf("For deletion")
+        val rowsAffected = contentResolver.delete(TasksContract.CONTENT_URI,
+            selection,
+            selectionArgs)
+
+        Log.d(TAG, "Number of rows deleted is $rowsAffected")
+    }
+
+    private fun testDelete() {
+
+        val taskUri = TasksContract.buildUriFromId(3)
+        val rowsAffected = contentResolver.delete(taskUri, null, null)
+        Log.d(TAG, "Number of rows deleted is $rowsAffected")
+    }
+
+    private fun testUpdateTwo() {
+        val values = ContentValues().apply {
+            put(TasksContract.Columns.TASK_NAME, 99)
+            put(TasksContract.Columns.TASK_DESCRIPTION, "Completed")
+        }
+
+        val selection = TasksContract.Columns.TASK_SORT_ORDER + " = 2"
+        val selectionArgs = arrayOf("999")
+        val rowsAffected = contentResolver.update(TasksContract.CONTENT_URI,
+            values,
+            selection,
+            selectionArgs)
+        Log.d(TAG, "Number of rows updated is $rowsAffected")
+    }
+
     private fun testUpdate() {
         val values = ContentValues().apply {
             put(TasksContract.Columns.TASK_NAME, "Content Provider")
@@ -69,6 +105,7 @@ class MainActivity : AppCompatActivity() {
         Log.d(TAG,"New row id (in uri) is $uri")
         Log.d(TAG, "id, (in uri) is ${TasksContract.getId(uri)}")
     }
+    //******* test functions ***********
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -81,7 +118,7 @@ class MainActivity : AppCompatActivity() {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.menumain_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
     }
